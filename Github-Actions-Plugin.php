c7d6622e76d4ff7020b504663a72c64fb3d4463b
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-11-03 11:48:18 +0300

    Plugin Descriptions

---
+++ Github-Actions-Plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package GithubActions
+ */
+/**
+ * Plugin Name: Github Actions
+ * Plugin URI: #
+ * Description: A WordPress plugin to input GitHub credentials and trigger GitHub Actions workflow.
+ * Version: 1.0.0
+ * Author: Wilson
+ * Author URI: #
+ * License: GPLv2 or later
+ * Text Domain: github-actions 
+ */
