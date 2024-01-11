ae6685102334ea928f57b5e8efe2fa23e2329cce
Wilson Mbuthia <wyllyjeremy@gmail.com>
2024-01-11 17:21:43 +0300

    Updating license

--- Github-Actions-Plugin.php
+++ Github-Actions-Plugin.php
@@ -1,16 +1,39 @@
 <?php
 /**
- * @package GithubActions
- */
-/**
- * Plugin Name: Github Actions
- * Plugin URI: https://github.com/Wyllymk/Github-Actions-Plugin/
- * Description: A WordPress plugin to input GitHub credentials and trigger GitHub Actions workflow.
- * Version: 1.0.0
- * Author: Wilson
- * Author URI: https://atomicwebspace.com/
- * License: GPLv2 or later
- * Text Domain: github-actions 
+ * @package             GithubActions
+ * @author              Wilson
+ * @copyright           2024 Kabatha
+ * 
+ * @wordpress-plugin
+ * Plugin Name:             Github Actions
+ * Plugin URI:              https://github.com/Wyllymk/Github-Actions-Plugin/
+ * Description:             A WordPress plugin to input GitHub credentials and trigger GitHub Actions workflow.
+ * Version:                 0.1.1
+ * Requires at least:       6.0
+ * Requires PHP:            7.2
+ * Author:                  Wilson
+ * Author URI:              https://wilsondevops.com/
+ * License:                 GPLv2 or later
+ * License URI:             https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:              https://example.com/my-plugin/
+ * Text Domain:             github-actions 
+ * Domain Path:             /languages
+
+*/
+
+/*
+Github Actions is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Github Actions is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Github Actions. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
 if( ! defined('ABSPATH')){
@@ -18,6 +41,7 @@ if( ! defined('ABSPATH')){
 }
 
 // Define Constants
+define( 'GITHUB_ACTIONS_PLUGIN_VERSION', '0.1.0');
 define( 'GITHUB_ACTIONS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'GITHUB_ACTIONS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'GITHUB_ACTIONS_PLUGIN_NAME', plugin_basename(__FILE__) );
