cfdc5b76d90feb845f1d2fdac04b694571078b8e
Wilson Mbuthia <wyllyjeremy@gmail.com>
2024-01-19 11:52:59 +0300

    Updater

--- Github-Actions-Plugin.php
+++ Github-Actions-Plugin.php
@@ -6,7 +6,7 @@
  * Plugin Name: Github Actions
  * Plugin URI: https://github.com/Wyllymk/Github-Actions-Plugin/
  * Description: A WordPress plugin to input GitHub credentials and trigger GitHub Actions workflow.
- * Version: 1.0.0
+ * Version: 0.1.0
  * Author: Wilson
  * Author URI: https://atomicwebspace.com/
  * License: GPLv2 or later
@@ -52,4 +52,33 @@ register_deactivation_hook(__FILE__, 'deactivateGithubActionsExternally');
 */
 if(class_exists('GAP\\Github_Actions_Init')){
     GAP\Github_Actions_Init::registerServices();
+}
+
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
+
+	include_once 'Github-Actions-Update.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+
+		$config = array(
+			'slug' => GITHUB_ACTIONS_PLUGIN_NAME,
+			'proper_folder_name' => dirname( GITHUB_ACTIONS_PLUGIN_NAME ),
+			'api_url' => 'https://api.github.com/repos/Wyllymk/Github-Actions-Plugin',
+			'raw_url' => 'https://raw.github.com/Wyllymk/Github-Actions-Plugin/production',
+			'github_url' => 'https://github.com/Wyllymk/Github-Actions-Plugin',
+			'zip_url' => 'https://github.com/Wyllymk/Github-Actions-Plugin/releases/download/v0.1.0/Github-Actions-Plugin.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+
+		new WP_GitHub_Updater( $config );
+
+	}
+
 }
